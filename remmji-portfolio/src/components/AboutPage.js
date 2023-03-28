a2a939da4ec708e0b5cd06ea534782714adeb12e
remmji <remek6@gmail.com>
2023-03-28 21:39:22 +0200

    sdasd

--- remmji-portfolio/src/components/AboutPage.js
+++ remmji-portfolio/src/components/AboutPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme } from './Themes'
-import styled from 'styled-components'
+import styled, {keyframes} from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import SocialIcons from '../subComponents/SocialIcons'
 import PowerButton from '../subComponents/PowerButton'
 import ParticlesComponent from '../subComponents/ParticlesComponent'
-
+import astronaut from '../assets/Images/spaceman.png'
 
 
 
@@ -15,9 +15,46 @@ background-color: ${props => props.theme.body};
 width: 100vw;
 height: 100vh;
 position: relative;
-display: flex;
-justify-content: space-evenly;
-align-items: center;
+overflow: hidden;
+`
+
+const float = keyframes`
+  0% {transform: translateY(-10px)}
+  50% {transform: translateY(15px) translateX(15px)}
+  100% {transform: translateY(-10px)}
+`;
+
+const Spaceman = styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+img{
+  width: 100%;
+  height: auto;
+}
+`
+
+const Main =  styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+  backdrop-filter: blur(4px);
+  
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
 `
 
 
@@ -31,6 +68,14 @@ const AboutPage = () => {
       <SocialIcons theme='dark'/>
       <PowerButton />
       <ParticlesComponent theme='dark'/>
+      <Spaceman>
+        <img src={astronaut} alt="spaceman" />
+      </Spaceman>
+      <Main>
+        Hi there, I'm Remy, a self-taught front-end developer from Poland with a passion for creating beautiful and responsive web applications.
+        <br/><br/>
+         I started my coding journey about six months ago, and since then, I've been fully immersed in learning everything I can about front-end development.
+      </Main>
     </Box>
     </ThemeProvider>
 
---
+++ remmji-portfolio/src/data/ProjectData.js
@@ -0,0 +1,61 @@
+export const ProjectsData = [
+    {
+        id:1,
+        name:"E-Commerce",
+        description:"Project build in React.It is a website which mimics an online shop.It includes features such as a functional shopping cart with add and remove functionality, a checkout form, a lightbox and animations.",
+        tags:["HTML","CSS","React JS","react-router","Framer Motion"],
+        
+        demo:"https://my-app-remmji.vercel.app/",
+        github:"https://github.com/remmji/frontend-mentor-2/tree/main/ecommerce-product-page-main"
+    },
+    {
+        id:2,
+        name:"Space Tourism",
+        description:"Space-Tourism is a visually appealing multi-page web application built with React.",
+        tags:["HTML","CSS","React Js","react-router"],
+        
+        demo:"https://remmji.github.io/",
+        github:"https://github.com/remmji/remmji.github.io/tree/main/space/space-tourism-app"
+        
+    },
+    {
+        id:3,
+        name:"",
+        description:"",
+        tags:["react"],
+        
+        demo:"",
+        github:""
+    },
+    {
+        id:4,
+        name:"",
+        description:"",
+        tags:[""],
+        
+        demo:"",
+        github:""
+       
+    },{
+        id:5,
+        name:"",
+        description:"",
+        tags:[""],
+        
+        demo:"",
+        github:""
+        
+    },
+    {
+        id:6,
+        name:"",
+        description:"",
+        tags:["react"],
+        
+        demo:"",
+        github:""
+        
+    },
+    
+  
+] 
