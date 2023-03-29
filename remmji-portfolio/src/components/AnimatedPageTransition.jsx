dfa5dfa743541a9df158ec7a4e8a874f05b29045
remmji <remek6@gmail.com>
2023-03-29 16:05:58 +0200

    Update AnimatedPageTransition.jsx

--- remmji-portfolio/src/components/AnimatedPageTransition.jsx
+++ remmji-portfolio/src/components/AnimatedPageTransition.jsx
@@ -3,8 +3,8 @@ import {motion} from "framer-motion";
 
 const animations = {
     initial: {opacity:0},
-    animate: {opacity:1, transition: {duration: 0.5, ease: "easeInOut"}},
-    exit: {opacity:0, transition: {duration: 0.3, ease: "easeInOut"}},
+    animate: {opacity:1, transition: {duration: 1, ease: "easeInOut"}},
+    exit: {opacity:0, transition: {duration: 0.5, ease: "easeInOut"}},
 }
 
 function AnimatedPageTransition({children}) {
