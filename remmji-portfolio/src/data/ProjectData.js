0dbd714e099ef5989a57e0ede144206860c4a946
remmji <remek6@gmail.com>
2023-04-06 11:53:44 +0200

    Update ProjectData.js

--- remmji-portfolio/src/data/ProjectData.js
+++ remmji-portfolio/src/data/ProjectData.js
@@ -3,7 +3,7 @@ export const ProjectsData = [
         id:1,
         name:"E-Commerce",
         description:"It is a website which mimics an online shop. It includes features such as a functional shopping cart with add and remove functionality, a checkout form, lightbox and smooth animations.",
-        tags:["HTML","CSS","React JS","react-router","Framer Motion"],
+        tags:["HTML","CSS","react","react-router","framer-motion"],
         
         demo:"https://my-app-remmji.vercel.app/",
         github:"https://github.com/remmji/frontend-mentor-2/tree/main/ecommerce-product-page-main"
@@ -12,7 +12,7 @@ export const ProjectsData = [
         id:2,
         name:"Space Tourism",
         description:"Space-Tourism is a visually appealing multi-page web application built in React.",
-        tags:["HTML","CSS","React Js","react-router", "Framer Motion"],
+        tags:["HTML","CSS","react","react-router", "framer-motion"],
         
         demo:"https://space-tourism-app-five.vercel.app",
         github:"https://github.com/remmji/remmji.github.io/tree/main/space/space-tourism-app"
@@ -20,12 +20,12 @@ export const ProjectsData = [
     },
     {
         id:3,
-        name:"",
-        description:"",
-        tags:["react"],
+        name:"REST Countries API",
+        description:"Country finder app where the user can search and filter countries, change the theme, navigate throughout the bordering countries, and connect with Google Maps.",
+        tags:["react","framer-motion","styled-components","react-router","REST API"],
         
-        demo:"",
-        github:""
+        demo:"https://countries-api-remmji.vercel.app/",
+        github:"https://github.com/remmji/countries-api"
     },
     {
         id:4,
